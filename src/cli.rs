e0c70e5a8b7ea2477569c33c1ff1c132288ae331
jakublaba <jakub.maciej.laba@gmail.com>
2024-07-05 18:41:33 +0200

    Add help docs to cli

--- src/cli.rs
+++ src/cli.rs
@@ -10,10 +10,13 @@ pub struct Cli {
 
 #[derive(Subcommand, Debug)]
 pub enum Cmd {
+    /// Switch profiles
     Su {
+        /// Name of the profile
         #[arg(value_parser = |name: &str| Profile::from_json(String::from(name)))]
         profile: Profile,
     },
+    /// Manage profiles
     Profile {
         #[clap(subcommand)]
         command: Option<ProfileCmd>,
@@ -22,20 +25,34 @@ pub enum Cmd {
 
 #[derive(Subcommand, Debug)]
 pub enum ProfileCmd {
+    /// Add a new profile
     Add {
+        /// Name of the profile
         #[arg(short, long)]
         name: String,
-        #[arg(short, long)]
-        email: String,
+        /// Git username (user.name in gitconfig)
+        #[arg(short, long = "username")]
+        user_name: String,
+        /// Git user email (user.email in gitconfig)
+        #[arg(short = 'e', long = "email")]
+        user_email: String,
     },
+    /// Remove an existing profile
     Remove {
+        /// Name of the profile
         #[arg()]
         profile: String,
     },
+    /// Edit an existing profile
     Edit {
+        /// Name of the profile
         #[arg(short, long)]
-        name: Option<String>,
-        #[arg(short, long)]
-        email: Option<String>,
+        name: String,
+        /// Git username (user.name in gitconfig)
+        #[arg(short, long = "username")]
+        user_name: Option<String>,
+        /// Git user email (user.email in gitconfig)
+        #[arg(short = 'e', long = "email")]
+        user_email: Option<String>,
     },
 }
