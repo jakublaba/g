ef563a559f7b4cf717bafbe549b595fd5f5d656f
Jakub Łaba <77542257+jakublaba@users.noreply.github.com>
2024-07-07 17:13:06 +0000

    Implement basic ssh features (#1)
    * Implement (most of) ssh features
    
    * Fix skill issue with writing ssh keys to files
    
    * Improve consistency of write key function signatures
    
    * Add method for generating randomarts
    
    * Implement custom SshError and update return types

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,26 @@
+use std::any::Any;
+
 use clap::Parser;
-use cli::Cli;
+use serde::ser::Error;
+
+use crate::model::Profile;
+use crate::ssh::{generate_keys, generate_randomart, write_private_key, write_public_key};
 
 mod cli;
 mod model;
+mod ssh;
 
 fn main() {
-    let cli = Cli::parse();
-    println!("{:#?}", cli);
+    // let cli = Cli::parse();
+    // println!("{:#?}", cli);
+    let profile = Profile {
+        name: String::from("johnsmith"),
+        user_name: String::from("John Smith"),
+        user_email: String::from("john.smith@example.com"),
+    };
+    let (priv_key, pub_key) = generate_keys(&profile);
+    let randomart = generate_randomart(&priv_key);
+    println!("{randomart}");
+    write_private_key(&profile, &priv_key).unwrap();
+    write_public_key(&profile, &pub_key).unwrap();
 }
