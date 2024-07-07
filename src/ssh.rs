3147b6d774814cd0aea9e60d5ac898458e59a945
jakublaba <jakub.maciej.laba@gmail.com>
2024-07-07 18:37:52 +0200

    Fix skill issue with writing ssh keys to files

--- src/ssh.rs
+++ src/ssh.rs
@@ -1,13 +1,8 @@
-// TODO improve error handling
-
-use std::fs::File;
-use std::io::{BufWriter, Write};
+use std::path::Path;
 
 use rand::thread_rng;
-use ssh_encoding::Encode;
-use ssh_key::private::{Ed25519Keypair, Ed25519PrivateKey};
-use ssh_key::public::Ed25519PublicKey;
-use ssh_key::sha2::{Digest, Sha256};
+use ssh_key::{LineEnding, PrivateKey, PublicKey};
+use ssh_key::private::Ed25519Keypair;
 
 use crate::model::Profile;
 
@@ -15,39 +10,27 @@ const SSH_KEYS_PATH: &str = "~/.ssh";
 
 // TODO implement custom SshError struct and change method signatures to Result<_, SshError>
 
-pub fn generate_keys() -> (Ed25519PrivateKey, Ed25519PublicKey) {
+pub fn generate_keys(profile: &Profile) -> (PrivateKey, PublicKey) {
     let mut rng = thread_rng();
     let pair = Ed25519Keypair::random(&mut rng);
+    let private = PrivateKey::from(pair);
+    let mut public = PublicKey::from(&private);
+    public.set_comment(&profile.user_email);
 
-    (pair.private, pair.public)
+    (private, public)
 }
 
-pub fn write_private_key(profile: &Profile, key: &Ed25519PrivateKey) {
-    let profile_name = &profile.name;
-    let file_name = private_key_path(profile_name);
-    let file = File::create(&file_name)
-        .expect(&format!("Error creating file: {file_name}"));
-    let mut buf_writer = BufWriter::new(file);
-    // todo figure out the encoding skill issue
-    let mut sha256_writer = Sha256::new();
-    key.encode(&mut sha256_writer)
-        .expect(&format!("Error encoding private key: {file_name}"));
-    buf_writer.write_all(key.as_ref())
-        .expect(&format!("Error writing private key: {file_name}"));
+pub fn write_private_key(profile_name: &str, key: &PrivateKey) {
+    let path = private_key_path(profile_name);
+    key.write_openssh_file(Path::new(&path), LineEnding::LF)
+        .expect(&format!("Error writing private key: {path}"));
 }
 
-pub fn write_public_key(profile: &Profile, key: &Ed25519PublicKey) {
+pub fn write_public_key(profile: &Profile, key: &PublicKey) {
     let profile_name = &profile.name;
-    let file_name = public_key_path(profile_name);
-    let file = File::create(&file_name)
-        .expect(&format!("Error creating file: {file_name}"));
-    let mut buf_writer = BufWriter::new(file);
-    let mut enc_writer = Sha256::new();
-    // todo figure out the encoding skill issue
-    key.encode(&mut enc_writer)
-        .expect(&format!("Error encoding public key: {file_name}"));
-    buf_writer.write_all(key.as_ref())
-        .expect(&format!("Error writing public key: {file_name}"));
+    let path = public_key_path(profile_name);
+    key.write_openssh_file(Path::new(&path))
+        .expect(&format!("Error writing public key: {path}"));
 }
 
 fn private_key_path(key_file_name: &str) -> String {
