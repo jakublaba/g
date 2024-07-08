2a30fd9fa519d6b726bbb0cbb57637d854a3e09e
jakublaba <jakub.maciej.laba@gmail.com>
2024-07-08 15:49:44 +0200

    Reorganize ssh module

---
+++ src/ssh/config.rs
@@ -0,0 +1,46 @@
+use std::collections::HashSet;
+use std::fs;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+use crate::ssh::{Result, SshError};
+
+const SSH_CONFIG_DIR: &str = "~/.ssh/config";
+
+pub fn add_config_entry(profile_name: &str) -> Result<()> {
+    let ssh_config_dir = ssh_config_dir();
+    let content = filtered_ssh_config(profile_name)? + &config_entry(profile_name);
+    fs::write(&ssh_config_dir, content)
+        .map_err(|_| SshError::from(format!("Error appending entry for profile {profile_name} to ssh config")))
+}
+
+fn filtered_ssh_config(excluded_profile_name: &str) -> Result<String> {
+    let ssh_config_dir = ssh_config_dir();
+    let file = File::open(&ssh_config_dir)
+        .map_err(|_| SshError::from(format!("Error opening ssh config: {ssh_config_dir}")))?;
+    let reader = BufReader::new(file);
+    let config_entry_lines = config_entry(excluded_profile_name)
+        .lines()
+        .map(String::from)
+        .collect::<HashSet<_>>();
+    let content = reader.lines()
+        .map(|r| r.unwrap())
+        .filter(|line| !config_entry_lines.contains(line))
+        .collect::<Vec<_>>()
+        .join("\n");
+
+    Ok(content)
+}
+
+fn ssh_config_dir() -> String {
+    String::from(shellexpand::tilde(SSH_CONFIG_DIR))
+}
+
+fn config_entry(profile_name: &str) -> String {
+    format!(r#"
+Host github.com-{profile_name}
+    HostName        github.com
+    User            git
+    IdentityFile    ~/.ssh/id_{profile_name}
+"#)
+}
---
+++ src/ssh/mod.rs
@@ -0,0 +1,30 @@
+use std::error::Error;
+use std::fmt::{Display, Formatter};
+
+pub mod config;
+pub mod key;
+
+pub type Result<T> = std::result::Result<T, SshError>;
+
+#[derive(Debug)]
+pub struct SshError(pub String);
+
+impl From<String> for SshError {
+    fn from(msg: String) -> Self {
+        Self { 0: msg }
+    }
+}
+
+impl From<&str> for SshError {
+    fn from(msg: &str) -> Self {
+        Self { 0: String::from(msg) }
+    }
+}
+
+impl Display for SshError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+impl Error for SshError {}
