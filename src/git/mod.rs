0015285f1f32ab5c5e83765baaedc6a8394c8ed3
Jakub Łaba <77542257+jakublaba@users.noreply.github.com>
2024-07-20 00:02:26 +0000

    Fix cloning issue (#9)

--- src/git/mod.rs
+++ src/git/mod.rs
@@ -8,6 +8,7 @@ use serde::de::IntoDeserializer;
 use serde_json::value::Index;
 
 use crate::git::error::Error;
+use crate::ssh::key::{private_key_path, public_key_path};
 
 pub mod error;
 
@@ -16,7 +17,6 @@ const URL_REGEX: &str = r"git@github\.com:.+\/(?<repo>.+)\.git";
 pub type Result<T> = std::result::Result<T, Error>;
 
 // TODO for now idk if the Repository return type is useful or not
-// TODO resolve cloning skill issue
 pub fn clone(profile_name: &str, url: &str) -> Result<Repository> {
     let repo_name = parse_repo_name(url)?;
     repo_builder(profile_name)
@@ -26,7 +26,12 @@ pub fn clone(profile_name: &str, url: &str) -> Result<Repository> {
 
 fn repo_builder(profile_name: &str) -> RepoBuilder {
     let mut callbacks = RemoteCallbacks::new();
-    callbacks.credentials(|_url, _usr, _types| Cred::ssh_key_from_agent(profile_name));
+    callbacks.credentials(|_url, usr, _types| Cred::ssh_key(
+        usr.unwrap(),
+        Some(Path::new(&public_key_path(profile_name))),
+        Path::new(&private_key_path(profile_name)),
+        None,
+    ));
     let mut fetch_options = FetchOptions::new();
     fetch_options.remote_callbacks(callbacks);
     let mut repo_builder = RepoBuilder::new();
