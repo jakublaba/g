f1288add792db54c8bff8f4ca291db07aa6a5dd0
jakublaba <jakub.maciej.laba@gmail.com>
2024-07-20 02:15:24 +0200

    Remove junk from main

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,14 @@
 use clap::Parser;
 use serde::ser::Error;
 
-use crate::git::clone;
-use crate::profile::profile::Profile;
+use crate::cli::Cli;
 
 mod cli;
 mod ssh;
 mod git;
 mod profile;
 
-// TODO (globally) improve error handling to also pass the underlying error messages
 fn main() {
-    // let cli = Cli::parse();
-    // println!("{:#?}", cli);
-    let profile = Profile {
-        name: String::from("jakublaba"),
-        user_name: String::from("jakublaba"),
-        user_email: String::from("jakub.maciej.laba@gmail.com"),
-    };
-    // let (priv_key, pub_key) = generate_pair(&profile.name);
-    // let randomart = randomart(&priv_key);
-    // println!("{randomart}");
-    // write_private_key(&profile.name, &priv_key).unwrap();
-    // write_public_key(&profile.name, &pub_key).unwrap();
-    clone(&profile.name, "git@github.com:jakublaba/git-multiaccount.git").unwrap();
+    let cli = Cli::parse();
+    println!("{:#?}", cli);
 }
