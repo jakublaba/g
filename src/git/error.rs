a123a684b1b6feb55b185fb4637ba3cd9ee5fd93
jakublaba <jakub.maciej.laba@gmail.com>
2024-07-20 03:03:08 +0200

    Fix git module errors

--- src/git/error.rs
+++ src/git/error.rs
@@ -5,13 +5,13 @@ use thiserror::Error;
 #[derive(Error, Debug)]
 pub enum Error {
     #[error("Git error: {0}")]
-    Git(#[from] git2::Error),
+    Git(git2::Error),
     #[error("Bad url: {0}")]
     Url(String),
     #[error("Can't read dir: {0}")]
-    Io(#[from] io::Error),
+    Io(io::Error),
     #[error("Can't open repo: {0}")]
-    Repo(#[from] git2::Error),
+    Repo(git2::Error),
     #[error("Can't obtain config: {0}")]
-    Config(#[from] git2::Error),
+    Config(git2::Error),
 }
