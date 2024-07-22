5a81df10c41219559e6046381d1976c8fffd19ef
jakublaba <jakub.maciej.laba@gmail.com>
2024-07-22 17:07:42 +0200

    Remove unused ssh::error variants

--- src/ssh/error.rs
+++ src/ssh/error.rs
@@ -1,5 +1,3 @@
-use std::io;
-
 use thiserror::Error;
 
 #[derive(Error, Debug)]
@@ -14,16 +12,4 @@ pub enum Error {
         key_path: String,
         cause: ssh_key::Error,
     },
-    #[error("IO error: {path}")]
-    Io {
-        path: String,
-        cause: io::Error,
-    },
-    #[error("Error interacting with ssh-agent at {ssh_auth_sock}")]
-    Agent {
-        ssh_auth_sock: String,
-        cause: ssh_agent_client_rs::Error,
-    },
-    #[error("SSH_AUTH_SOCK environment variable is not set")]
-    SshAuthSock,
 }
