5c32f6ccb4fae6a3e17912888211fbcc94919fb9
jakublaba <jakub.maciej.laba@gmail.com>
2024-07-31 21:33:27 +0200

    Add custom errors to cli module

---
+++ src/cli/error.rs
@@ -0,0 +1,11 @@
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub(crate) enum Error {
+    #[error(transparent)]
+    Git(#[from] crate::git::error::Error),
+    #[error(transparent)]
+    Profile(#[from] crate::profile::error::Error),
+    #[error(transparent)]
+    Ssh(#[from] crate::ssh::error::Error),
+}
