e76ff101448be699b2da7ce8929cd639c648eb5b
jakublaba <jakub.maciej.laba@gmail.com>
2024-08-12 20:05:13 +0200

    Add tests for crate::ssh::key

--- src/ssh/key/mod.rs
+++ src/ssh/key/mod.rs
@@ -1,8 +1,8 @@
 use std::path::Path;
 
 use rand::thread_rng;
-use ssh_key::{LineEnding, PrivateKey, PublicKey};
 use ssh_key::private::{DsaKeypair, Ed25519Keypair, RsaKeypair};
+use ssh_key::{LineEnding, PrivateKey, PublicKey};
 
 use crate::home;
 use crate::ssh::error::Error;
@@ -39,9 +39,7 @@ impl From<KeyPair> for PrivateKey {
 pub fn pair(email: &str, key_type: &KeyType) -> Result<(PrivateKey, PublicKey)> {
     let mut rng = thread_rng();
     let pair = match key_type {
-        KeyType::Dsa => {
-            KeyPair::Dsa(DsaKeypair::random(&mut rng)?)
-        }
+        KeyType::Dsa => KeyPair::Dsa(DsaKeypair::random(&mut rng)?),
         KeyType::Rsa { size } => {
             let size = size.unwrap_or(DEFAULT_RSA_SIZE);
             if size < MIN_RSA_SIZE {
@@ -49,7 +47,7 @@ pub fn pair(email: &str, key_type: &KeyType) -> Result<(PrivateKey, PublicKey)> 
             }
             KeyPair::Rsa(RsaKeypair::random(&mut rng, size)?)
         }
-        KeyType::Ed25519 => KeyPair::Ed25519(Ed25519Keypair::random(&mut rng))
+        KeyType::Ed25519 => KeyPair::Ed25519(Ed25519Keypair::random(&mut rng)),
     };
     let private = PrivateKey::from(pair);
     let mut public = PublicKey::from(&private);
@@ -96,4 +94,60 @@ pub(crate) fn path_public(profile_name: &str) -> String {
 
 fn ssh_dir() -> String {
     format!("{}/.ssh", home())
+}
+
+#[cfg(test)]
+mod test {
+    use crate::ssh;
+    use crate::ssh::error::Error;
+    use crate::ssh::key::r#type::KeyType;
+    use rand::thread_rng;
+    use rstest::rstest;
+    use spectral::assert_that;
+    use spectral::prelude::PathAssertions;
+    use ssh_key::{Algorithm, PrivateKey, PublicKey};
+    use std::{env, fs};
+    use tempfile::tempdir;
+
+    const PROFILE_NAME: &str = "test";
+    const EMAIL: &str = "test@email.com";
+
+    mod pair {
+        use super::*;
+
+        #[rstest]
+        #[case::dsa(KeyType::Dsa, Algorithm::Dsa)]
+        #[case::rsa_default_size(KeyType::Rsa{ size: None }, Algorithm::Rsa{ hash: None })]
+        #[case::rsa_custom_size(KeyType::Rsa{ size: Some(2048) }, Algorithm::Rsa{ hash: None })]
+        #[case::ed25519(KeyType::Ed25519, Algorithm::Ed25519)]
+        fn ok(#[case] key_type: KeyType, #[case] alg: Algorithm) {
+            let (private, public) = ssh::key::pair(EMAIL, &key_type).unwrap();
+
+            assert_that!(private.algorithm()).is_equal_to(alg);
+            assert_that!(public.key_data()).is_equal_to(PublicKey::from(private).key_data());
+            assert_that!(public.comment()).is_equal_to(EMAIL);
+        }
+
+        #[test]
+        fn err() {
+            let size = 2047;
+            let key_type = KeyType::Rsa { size: Some(size) };
+            let err = ssh::key::pair(EMAIL, &key_type).unwrap_err();
+            let expected_err = Error::InvalidRsaLength(size);
+
+            assert_that!(format!("{err}")).is_equal_to(format!("{expected_err}"));
+        }
+    }
+
+    #[test]
+    fn write_private() {
+        let fake_home = tempdir().unwrap();
+        env::set_var("HOME", fake_home.path());
+        fs::create_dir(fake_home.path().join(".ssh")).unwrap();
+        let private = PrivateKey::random(&mut thread_rng(), Algorithm::Ed25519).unwrap();
+
+        ssh::key::write_private(PROFILE_NAME, &private).unwrap();
+
+        assert_that!(fake_home.path().join(format!(".ssh/id_{PROFILE_NAME}"))).exists();
+    }
 }
