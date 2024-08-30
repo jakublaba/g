6a7ad7d65955a4f72d24feaf8274de7856600b3a
jakublaba <jakub.maciej.laba@gmail.com>
2024-08-30 10:32:15 +0200

    Fix buildscript logic

--- build.rs
+++ build.rs
@@ -1,27 +1,33 @@
 use serde::{Deserialize, Serialize};
-use std::collections::HashMap;
 use std::fs;
-use std::hash::{DefaultHasher, Hash, Hasher};
 
 const HOME: &str = env!("HOME");
 const PROFILES_DIR: &str = ".config/g-profiles";
 
 fn main() {
-    let profiles = list_profiles()
+    let profile_names = list_profiles();
+    if !profile_names.is_empty() {
+        println!("cargo::warning=Detected json profiles");
+        profile_names
+            .iter()
+            .for_each(|n| println!("cargo::warning={n}"));
+        println!("cargo::warning=They've been migrated to binary format to ensure compatibility with v2.x.x")
+    };
+    let profiles = profile_names
         .into_iter()
         .map(|profile_name| Profile::read_json(&profile_name))
         .collect::<Vec<_>>();
-    // todo load the cache to avoid destroying it
-    let mut cache = HashMap::new();
+    let mut cache = cache::load();
     profiles.iter().for_each(|p| {
-        let key = key(&p.username, &p.email);
+        let key = cache::key(&p.username, &p.email);
         cache.insert(key, p.name.to_string());
     });
-    let cache_path = format!("{HOME}/{PROFILES_DIR}/.cache");
-    let cache_bytes = bincode::serialize(&cache).unwrap();
-    // fs::write(cache_path, &cache_bytes[..]).unwrap();
-    // todo for some reason this doesn't work
-    profiles.into_iter().for_each(Profile::save_binary);
+    cache::save(cache);
+    profiles.into_iter().for_each(|profile| {
+        let name = profile.name.clone();
+        profile.save_binary();
+        fs::remove_file(format!("{HOME}/{PROFILES_DIR}/{name}.json")).unwrap()
+    });
 }
 
 fn list_profiles() -> Vec<String> {
@@ -29,9 +35,9 @@ fn list_profiles() -> Vec<String> {
         .unwrap()
         .map(|dir_entry| dir_entry.unwrap().path())
         .filter(|path| path.is_file())
-        .filter(|path| path.ends_with(".json"))
+        .filter(|path| path.extension().is_some())
+        .filter(|path| path.extension().unwrap() == "json")
         .map(|path| path.file_stem().unwrap().to_string_lossy().to_string())
-        .filter(|name| !name.starts_with('.'))
         .collect()
 }
 
@@ -82,15 +88,40 @@ impl Profile {
     fn save_binary(self) {
         let (profile_name, partial) = self.clone().into();
         let path = format!("{HOME}/{PROFILES_DIR}/{profile_name}");
-        let bytes = serde_json::to_vec(&partial).unwrap();
+        let bytes = bincode::serialize(&partial)
+            .unwrap_or_else(|err| panic!("Can't save profile {}, {err}", self.name));
         fs::write(&path, &bytes[..]).unwrap();
     }
 }
 
-fn key(username: &str, email: &str) -> u64 {
-    let mut hasher = DefaultHasher::new();
-    username.hash(&mut hasher);
-    email.hash(&mut hasher);
+mod cache {
+    use crate::{HOME, PROFILES_DIR};
+    use std::collections::HashMap;
+    use std::fs;
+    use std::hash::{DefaultHasher, Hash, Hasher};
+    use std::path::Path;
 
-    hasher.finish()
+    pub(super) fn load() -> HashMap<u64, String> {
+        let path = format!("{HOME}/{PROFILES_DIR}/.cache");
+        if !Path::new(&path).exists() {
+            return HashMap::new();
+        }
+        let bytes = fs::read(&path).unwrap_or_else(|err| panic!("Can't load cache: {err}"));
+
+        bincode::deserialize(&bytes[..]).expect("Can't deserialize cache")
+    }
+
+    pub(super) fn save(cache: HashMap<u64, String>) {
+        let path = format!("{HOME}/{PROFILES_DIR}/.cache");
+        let bytes = bincode::serialize(&cache).expect("Can't serialize cache");
+        fs::write(path, &bytes[..]).unwrap_or_else(|err| panic!("Can't save cache: {err}"));
+    }
+
+    pub(super) fn key(username: &str, email: &str) -> u64 {
+        let mut hasher = DefaultHasher::new();
+        username.hash(&mut hasher);
+        email.hash(&mut hasher);
+
+        hasher.finish()
+    }
 }
